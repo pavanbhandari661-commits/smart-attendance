796a61b445c544a106bd2ed415f918896057ee3a
Pavan Bhandari <pavanbhandari661@gmail.com>
2026-09-14 01:42:12 +0530

    Connect backend to MongoDB Atlas

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,14 @@
 require("dotenv").config();
+
 const express = require("express");
+const connectDB = require("./config/db");
 
 const app = express();
 
 const PORT = process.env.PORT || 5000;
+
+// Connect to MongoDB
+connectDB();
 
 app.get("/", (req, res) => {
     res.send("SmartAttend AI Backend is running!");
