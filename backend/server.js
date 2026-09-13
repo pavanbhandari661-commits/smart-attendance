d518ec5bc0f2de398587a577cf25e2bad2dadb8e
Pavan Bhandari <pavanbhandari661@gmail.com>
2026-09-14 00:23:17 +0530

    Configure environment variables

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,9 @@
+require("dotenv").config();
 const express = require("express");
 
 const app = express();
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.get("/", (req, res) => {
     res.send("SmartAttend AI Backend is running!");
